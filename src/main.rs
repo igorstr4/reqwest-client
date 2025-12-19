919b92564cd730f8be2705319fd0804a880d9f23
igor <igor.str4@gmail.com>
2025-12-19 19:08:11 +0100

    Reqwest: get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let http_client = reqwest::ClientBuilder::new()
+        .connection_verbose(true)
+        .build()
+        .expect("Http client building error");
+
+    let response = http_client.get("https://dummyjson.com/test")
+        .send()
+        .await
+        .expect("HTTP GET request error");
+
+    let json: serde_json::Value = response.json().await.expect("JSON error");
+
+    println!("Response: {json:?}");
 }
