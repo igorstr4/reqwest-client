05806019870d4b00bf915760e24ba9c9ce1f8a11
igor <igor.str4@gmail.com>
2025-12-19 21:01:19 +0100

    Tracing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
-#[tokio::main]
-async fn main() {
+use thiserror::Error;
+use tracing_subscriber::{
+    layer::SubscriberExt,
+    util::{SubscriberInitExt, TryInitError},
+};
 
-    env_logger::init();
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Tracing initialization error {0}")]
+    TracingInitError(#[from] TryInitError),
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init()?;
+
+    let span = tracing::span!(tracing::Level::DEBUG, "span");
+
+    let _entered = span.enter();
+
+    tracing::info!("Before API call");
 
     let http_client = reqwest::ClientBuilder::new()
         .connection_verbose(true)
         .build()
         .expect("Http client building error");
 
-    let response = http_client.get("https://dummyjson.com/test")
+    let response = http_client
+        .get("https://dummyjson.com/test")
         .send()
         .await
         .expect("HTTP GET request error");
@@ -16,4 +37,8 @@ async fn main() {
     let json: serde_json::Value = response.json().await.expect("JSON error");
 
     println!("Response: {json:?}");
+
+    tracing::info!("After API call");
+
+    Ok(())
 }
